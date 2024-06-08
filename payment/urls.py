b7766260da13f45bbb099206073dc5eb067e9f54
Ning <a24701770@gmail.com>
2024-06-08 22:37:24 +0800

    add: home page

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = "payment"
+
 urlpatterns = [
-  path('', views.index)
+  path('', views.index),
+  path('/checkout', views.checkout, name="checkout")
 ]
