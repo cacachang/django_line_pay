32fb0d740997e5f97a242d8051e33d89684fa6d6
Ning <a24701770@gmail.com>
2024-06-08 21:51:28 +0800

    add: payment app

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
